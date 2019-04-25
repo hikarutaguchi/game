8b2d1b11730a3d508a1c9d4c3747b048e80993c3
taguchi_hikaru <hika19991027@icloud.com>
2019-04-25 09:50:03 +0900

    o

--- memory remind2/ModeScene.cpp
+++ memory remind2/ModeScene.cpp
@@ -8,7 +8,10 @@ ModeScene::ModeScene()
 	DrawBox(0, 0, 200, 200, 0xffff0, true);
 	if (CheckHitKey(KEY_INPUT_A) != 0)
 	{
+<<<<<<< HEAD
 		//asd
+=======
+>>>>>>> ea93142a8a04ca9fd7a109912d2cc546108e880c
 		std::make_unique<GameScene>();
 	}
 	ScreenFlip();
