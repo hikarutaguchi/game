f657e3eec3a930279ba7a9121ae481c7ed0d57cb
ozaki_jun <ozaki_jun_123@icloud.com>
2019-05-09 16:06:27 +0900

    a

---
+++ memory remind/ResultScene.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "BaseScene.h"
+#include "classObj.h"
+
+class ResultScene :
+	public BaseScene
+{
+public:
+	ResultScene();
+	~ResultScene();
+	virtual unique_Base Updata(unique_Base own, const Game_ctr &controller);	// 純粋仮想関数UpDate
+
+private:
+	int Init(void);
+	bool nextScene;
+	void Draw();
+	sharedListObj objList;			// ｲﾝｽﾀﾝｽした情報を登録
+	int Pad[4];						// パッド情報
+	bool padFlag;
+};
+
