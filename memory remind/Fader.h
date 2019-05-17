4bfca3cbac56226d88cc28fbbd2d2079a2c6e11d
ozaki_jun <ozaki_jun_123@icloud.com>
2019-05-17 12:19:19 +0900

    as

---
+++ memory remind/Fader.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define lpFader Fader::GetInstance()
+
+enum FADE_STATE
+{
+	FADE_IN_STARE,		//フェードイン開始
+	FADE_IN_END,		//フェードイン終了
+	FADE_OUT_STARE,		//フェードアウト開始
+	FADE_OUT_END,		//フェードアウト終了
+	FADE_WAIT			//待機(用途なし)
+};
+
+class Fader
+{
+public:
+	static Fader &GetInstance(void)
+	{
+		static Fader s_instance;
+		return s_instance;
+	}
+	void Updata();
+	void SetFadeIn(int fadeSpeed);	//フェードインセット
+	void SetFadeOut(int fadeSpeed);	//フェードアウトセット
+	int Init();
+	FADE_STATE GetFadeState();
+	Fader();
+	~Fader();
+
+private:
+	FADE_STATE fadeState;	//フェードの状態管理
+
+	int brightness;		//輝度
+	int fadeSpeed;		//フェードする速さ
+	bool fadeInFlag;	//フェードインフラグ
+	bool fadeOutFlag;	//フェードアウトフラグ
+};
