77e4afcf89e6903b78068938a76f6b507095f838
ozaki_jun <ozaki_jun_123@icloud.com>
2019-05-31 11:33:35 +0900

    線消し

--- memory remind/GameScene.cpp
+++ memory remind/GameScene.cpp
@@ -115,19 +115,7 @@ bool GameScene::GameDraw(void)
 	VECTOR2 tmp1(0, 0);
 	VECTOR2 tmp2(0, GAME_SCREEN_SIZE_Y);
 	VECTOR2 offset(lpSceneMng.GetDrawOffset());
-	for (; tmp1.x <= GAME_SCREEN_SIZE_X; tmp1.x += CHIP_SIZE)
-	{
-		tmp2.x = tmp1.x;
-		DrawLine(tmp1 + offset, tmp2 + offset, 0xffffff, true);
-	}
-	tmp1 = VECTOR2(0, 0);
-	tmp2.x = GAME_SCREEN_SIZE_X;
 
-	for (; tmp1.y <= GAME_SCREEN_SIZE_Y; tmp1.y += CHIP_SIZE)
-	{
-		tmp2.y = tmp1.y;
-		DrawLine(tmp1 + offset, tmp2 + offset, 0xffffff, true);
-	}
 	//std::for_each((objList)->begin(), (objList)->end(), [](std::shared_ptr<Obj> &itr) {itr->Draw(); });
 
 	DrawGraph(SCREEN_SIZE_X / 2 - 100, 0, lpImageMng.GetID("image/kannbann.png")[0], true);
